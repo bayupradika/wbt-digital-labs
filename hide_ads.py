42793d942f80d5677b12c7469349b4648cf1f44c
bayupradika <bayupromage@gmail.com>
2026-09-02 04:58:06 +0700

    style: temporarily comment out ad placeholders to prevent UI disruption until AdSense is approved

---
+++ hide_ads.py
@@ -0,0 +1,22 @@
+﻿import re
+
+with open('d:/Produk-Sell/tools/PDF-Toolkit-Lite/index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Comment out the AdSense Placeholders
+old_html = '''<!-- AdSense Placeholders -->
+<div class="wbt-ad-container wbt-ad-left">Iklan Kiri<br>(160x600)</div>
+<div class="wbt-ad-container wbt-ad-right">Iklan Kanan<br>(160x600)</div>
+<div class="wbt-ad-container wbt-ad-bottom">Banner Iklan Bawah (Responsive)</div>'''
+
+new_html = '''<!-- AdSense Placeholders (Dinonaktifkan Sementara) 
+<div class="wbt-ad-container wbt-ad-left">Iklan Kiri<br>(160x600)</div>
+<div class="wbt-ad-container wbt-ad-right">Iklan Kanan<br>(160x600)</div>
+<div class="wbt-ad-container wbt-ad-bottom">Banner Iklan Bawah (Responsive)</div>
+-->'''
+
+content = content.replace(old_html, new_html)
+
+with open('d:/Produk-Sell/tools/PDF-Toolkit-Lite/index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Berhasil menyembunyikan slot iklan.")
