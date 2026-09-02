0e49b622bf71fde354a3cd0e8a3adc3c454e5ab2
bayupradika <bayupromage@gmail.com>
2026-09-03 04:17:03 +0700

    style: remove redundant download-banner section as download links are now integrated into the pricing cards

---
+++ remove_download_banner.py
@@ -0,0 +1,10 @@
+﻿import re
+
+with open('d:/Produk-Sell/tools/PDF-Toolkit-Lite/index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Remove the entire download-section
+content = re.sub(r'<!-- Download App Section -->.*?</section>', '', content, flags=re.DOTALL)
+
+with open('d:/Produk-Sell/tools/PDF-Toolkit-Lite/index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
